e45b4f18d66cf50874487448644cf63860f5f1a2
dadamski-atos <54619272+dadamski-atos@users.noreply.github.com>
2020-10-08 14:00:03 +0200

    Update makeindex.go

--- src/makeindex.go
+++ src/makeindex.go
@@ -16,7 +16,7 @@ func main() {
 	s := `<html>
 <body>
 <h2>Hello `+ v +` the Tomcat server is running!</h2>
-<h1>The curent server time is <%= new java.util.Date() %></h1>
+<h1>The curent server time is <%=new java.util.Date()%></h1>
 </body>
 </html>`
 	fmt.Fprintf(file, s)
